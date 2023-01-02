24e4e414f551485fe768f0ee33457a1b61361a4c
June222 <38900003+June222@users.noreply.github.com>
2023-01-03 08:29:17 +0900

    const 설명

---
+++ dart_test/const.dart
@@ -0,0 +1,10 @@
+/*                      CONST
+* 뜻            : Compile-Time Variable
+* 주의          : final과 다른 의미를 가짐
+* Compile-Time  : 앱스토어에 올리기 전에 사용되는 immutable 변수
+ */
+
+void main() {
+  const max_allowed_value = 123; // Compile-Time Variable
+  final API = getAPIFromUser(); // 사용자에게 요청 받아 쓰는 것은 non-Compile-Time
+}
