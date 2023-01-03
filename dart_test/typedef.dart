63258c8bf2c411eb939958ec70db5ff57c5540fb
June222 <38900003+June222@users.noreply.github.com>
2023-01-03 21:55:10 +0900

    typedef는 alias가 앞으로 나옴

--- dart_test/typedef.dart
+++ dart_test/typedef.dart
@@ -1,6 +1,6 @@
 /**                                   TYPEDEF
  * 뜻 : 길거나 혹은 알아보기 힘든 변수명의 alias를 부여하는 것
- * 사용 방법 : typedef _LongTypeName = _ShortTypeName;
+ * 사용 방법 : typedef _ShortTypeName = _LongTypeName;
  * 사용 예시 : typedef MapSLI = Map<String,List<int>>;
  */
 typedef ListOfInt = List<int>;
