42cdee11331eadc010e8fcd20253c38f7655362a
June222 <38900003+June222@users.noreply.github.com>
2023-01-06 15:48:24 +0900

    주석: Code Action 설명 추가

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -51,11 +51,25 @@
  * (customized된 색을 쓰고 싶을 때) : color: Color(RGBO(#,#,#,$)) #는 순서대로 RGB 값, $는 투명도 값(0~1)
  */
 
-/// [Container]
-/// 뜻 : 하나의 개별적인 요소를 만들 때 쓰는 Widget
-/// 사용 상황 : 버튼처럼 하나의 독립적인 기능을 하는 요소를 만들고 싶을 때 씀.
-/// 사용 예시 : child: Container(_property: ..., child: _Widget)
-/// */
+// ignore: slash_for_doc_comments
+/**                [Container]
+ * 뜻 : 하나의 개별적인 요소를 만들 때 쓰는 Widget
+ * 사용 상황 : 버튼처럼 하나의 독립적인 기능을 하는 요소를 만들고 싶을 때 씀.
+ * 사용 예시 : child: Container(_property: ..., child: _Widget)
+ */
+
+///                          [Code Action]
+/// 뜻 : 코드를 리팩토링하는 간편한 방법
+/// 사용 상황 : Padding과 같이 여러 Widget들을 둘러싸는 Widget 코드를 적으려 할 때,
+///
+/// 1. const 자동 입력
+/// 사용 방법 : 1. Ctrl + Shift + p, 2. Open User Settings (JSON), 3. "editor.codeActionsOnSave":{"source.fixAll":true}, 추가
+/// 의미 : 저장(Ctrl + s)시에 const가 들어갈 수 있는 변수에는 const를 자동으로 넣어 compile 시간을 줄여준다.
+///
+/// 2. Widget Tree 만들기
+/// 사용 방법 : 1. Ctrl + Shift + p, 2. Open User Settings (JSON), 3. "dart.previewFlutterUiGuides": true, 추가
+/// 의미 : Widget Tree를 시각적으로 표현하여, 코드의 이해를 돕는다.
+///
 
 import 'package:flutter/material.dart';
 
@@ -164,7 +178,7 @@ class App extends StatelessWidget {
                       padding:
                           EdgeInsets.symmetric(vertical: 20, horizontal: 50),
                       child: Text(
-                        "Requst",
+                        "Request",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
