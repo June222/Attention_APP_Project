1114ae476697f754d67d82c7fc77559c2811001e
June222 <38900003+June222@users.noreply.github.com>
2023-01-07 00:26:22 +0900

    주석 : IconButton 설명 추가

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -8,6 +8,15 @@
 /// 생성 방법 : st 입력 후 자동완성 기능을 이용하여 Flutter StatefulWidget 선택
 /// 구성 : 1. Widget 자체(코드 변경 필요 없음), 2. State(보여줄 UI, Data를 여기에 작성)
 ///
+
+///                   [IconButton]
+/// 뜻 : 사용자의 입력을 통해 State를 수정할 수 있는 Button Widget
+/// [주의] : button은 Data를 변경하는 것이지만, UI의 State를 변경하지는 못한다.
+///   => 따로 refresh를 해주어야함.
+/// 사용 상황 : 사용자가 클릭하여 UI나 Data에 변경을 주고 싶을 때,
+/// 사용 예시 : child: IconButton(onPressed: _functionName, icon: Icon(Icons._iconName, ..), ..)
+///
+
 import "package:flutter/material.dart";
 
 void main() {
