3495eefb03c4cd44a5c58d8bc0a42e53ba5a0a1b
June222 <38900003+June222@users.noreply.github.com>
2023-01-07 21:27:44 +0900

    주석: as, package의 이름 정하기

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -1,3 +1,16 @@
+///                       [as]
+/// 뜻 : package의 이름 정하기
+/// 사용 상황 :
+///   import해온 package의 이름없이 method를 쓰는 것이
+///   가독성이나 이해도면에서 떨어질 때
+///
+/// 사용 예시 1(선언) : import 'package/...' as http;
+/// 사용 예시 2(사용) : _response = get(); => _response = http.get();
+///   url package의 get()이라는 method가 있는데 method만 쓸 때
+///   method의 역할을 직관적으로 이해하기 힘듬
+///     => package의 이름을 http라고하여 http.get()으로 사용
+///
+
 import 'package:http/http.dart' as http;
 
 class ApiService {
