b597667b83a9af1e2e18fd269008906e0c601668
June222 <38900003+June222@users.noreply.github.com>
2023-01-10 06:53:51 +0900

    주석 : Future 사용방법 1 설명

--- toonflix/lib/toonScreens/toon_screens.dart
+++ toonflix/lib/toonScreens/toon_screens.dart
@@ -1,3 +1,11 @@
+///                           [Futuer<T>]
+/// 사용 방법 1 :
+///   1. StatelessWidget을 StatefulWidget으로 바꾼다.
+///   2. Future<T>를 호출하는 Method를 담는 함수를 async로 정의한다.
+///   3. 정의한 함수 안에 setState()도 넣는다.
+///   4. initState() 안에 정의한 함수를 넣는다. (이때는 async가 필요없다.)
+///
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
