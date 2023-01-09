756e5892a32c379c220182b926fa6699e46d2604
June222 <38900003+June222@users.noreply.github.com>
2023-01-10 07:10:30 +0900

    #6.6 코드 : Future의 상태를 확인하는 function

--- toonflix/lib/toonScreens/toon_screens.dart
+++ toonflix/lib/toonScreens/toon_screens.dart
@@ -10,47 +10,34 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class ToonScreen extends StatefulWidget {
-  const ToonScreen({super.key});
+class ToonScreen extends StatelessWidget {
+  ToonScreen({super.key});
 
-  @override
-  State<ToonScreen> createState() => _ToonScreenState();
-}
-
-class _ToonScreenState extends State<ToonScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService().getTodaysToons();
-    isLoading = true;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.greenAccent.shade400,
-          title: const Text(
-            "오늘의 웹툰!",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.greenAccent.shade400,
+            title: const Text(
+              "오늘의 웹툰!",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
             ),
           ),
-        ),
-      ),
+          body: FutureBuilder(
+            builder: (context, snapshot) {
+              return snapshot.hasData
+                  ? const Text("There is Data now.")
+                  : const Text("Loading...");
+            },
+            future: webtoons,
+          )),
     );
   }
 }
