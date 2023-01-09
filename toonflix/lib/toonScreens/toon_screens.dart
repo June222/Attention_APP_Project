8403ac9395c4f9046cb7f005ff04373a8e086988
June222 <38900003+June222@users.noreply.github.com>
2023-01-10 07:49:44 +0900

    #6.7 ListView 코드, 주석

--- toonflix/lib/toonScreens/toon_screens.dart
+++ toonflix/lib/toonScreens/toon_screens.dart
@@ -16,8 +16,7 @@
 /// builder :
 ///   argument로 (BuildContext) context, snapshot을 받는다. (자동)
 ///   - context : BuildContext
-///   - snapshot : Future<T> 의 상태
-///
+
 /// future :
 ///   기다려야하는 대상 (callback 함수의 return 값)
 ///
@@ -28,6 +27,23 @@
 /// )
 ///
 
+///                         [ListView]
+/// 뜻 :
+///   여러 Widget들을 한꺼번에 띄워주는 Widget
+///
+/// 의미 :
+///   스크롤을 많이하는 화면에는 Row나 Column 대신 ListView를 쓴다.
+///
+/// 장점 :
+///   모든 children에 들어가는 Widget을 한꺼번에 보여주기 때문에
+///   적은 양에서는 버벅임이 없다.
+/// 단점 :
+///   인스타그램과 같이 무한스크롤이 되는 화면에서는 메모리를 많이 써서
+///   기기가 죽게 만들 수 있다.
+///
+/// 사용 예시 :
+///   Row나 Column과 동일일
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
@@ -35,7 +51,7 @@ import 'package:toonflix/services/api_service.dart';
 class ToonScreen extends StatelessWidget {
   ToonScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -55,8 +71,14 @@ class ToonScreen extends StatelessWidget {
         body: FutureBuilder(
           builder: (context, snapshot) {
             return snapshot.hasData
-                ? const Text("There is Data now.")
-                : const Text("Loading...");
+                ? ListView(
+                    children: [
+                      for (var webtoon in snapshot.data!) Text(webtoon.title),
+                    ],
+                  )
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  );
           },
           future: webtoons,
         ),
