7e8a78cfa9922255c151fc5833c368cfa0f98007
June222 <38900003+June222@users.noreply.github.com>
2023-01-10 17:50:36 +0900

    #6.15 코드 : url Data로 버튼 만들기

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -42,36 +42,120 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 5,
-                        offset: Offset(5, 10),
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 5,
+                            offset: Offset(5, 10),
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      width: 150,
+                      child: Image.network(widget.thumb),
+                    ),
                   ),
-                  width: 200,
-                  child: Image.network(widget.thumb),
-                ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder(
+                  future: webtoon,
+                  builder: ((context, snapshot) {
+                    if (snapshot.hasData) {
+                      var webtoon = snapshot.data!;
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            webtoon.about,
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "${webtoon.age} / ${webtoon.genre}",
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Text("...");
+                    }
+                  })),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: ((context, snapshot) {
+                  if (snapshot.hasData) {
+                    return episodeButtons(snapshot);
+                  } else {
+                    return Container();
+                  }
+                }),
               ),
             ],
           ),
-        ],
+        ),
       ),
+    );
+  }
+
+  Column episodeButtons(AsyncSnapshot<List<WebtoonEpisodeModel>> snapshot) {
+    return Column(
+      children: [
+        for (var episode in snapshot.data!)
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              color: Colors.green.shade300,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    episode.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const Icon(
+                    Icons.chevron_right_rounded,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
