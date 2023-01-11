54d9322793766b4392824292eafb10ce5bb386ec
June222 <38900003+June222@users.noreply.github.com>
2023-01-11 17:49:20 +0900

    #6.16 코드: launchUrl

--- toonflix/lib/widgets/episode_widget.dart
+++ toonflix/lib/widgets/episode_widget.dart
@@ -1,1 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
+import '../models/webtoon_epsiode_model.dart';
+
+class Episode extends StatelessWidget {
+  final String webtoon_id;
+  final WebtoonEpisodeModel episode;
+
+  const Episode({
+    Key? key,
+    required this.episode,
+    required this.webtoon_id,
+  }) : super(key: key);
+
+  onButtonTap() async {
+    // final url = Uri.parse("https://google.com");
+    // await launchUrl(url);
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoon_id&no=${episode.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          color: Colors.green.shade300,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 5,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
